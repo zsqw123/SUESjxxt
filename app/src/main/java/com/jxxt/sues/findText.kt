c0721db5b4338733af227dc16915e483176a765b
zsqw123 <3206279058@qq.com>
2019-08-04 08:51:20 +0800

    ~~

--- app/src/main/java/com/jxxt/sues/findText.kt
+++ app/src/main/java/com/jxxt/sues/findText.kt
@@ -6,14 +6,14 @@ import org.jsoup.nodes.Document
 import java.lang.StringBuilder
 
 class FindContext {
-    //html获取id URL
+    //html获取id URL by 正则
     fun findText(a: String): String {
         val regex = Regex("""<iframe src="(.*)ignoreHead=1""")
         val result = regex.find(a) ?: return "no"
         return "http://jxxt.sues.edu.cn/eams/" + result.groupValues[1].replace("&amp;", "&") + "ignoreHead=1"
     }
 
-    //HTML to Map
+    //HTML to Map by Jsoup
     fun resolveClasses(html: String): MutableMap<String, String> {
         val doc: Document = Jsoup.parse(html)
         val map: MutableMap<String, String> = mutableMapOf("99" to "test")
