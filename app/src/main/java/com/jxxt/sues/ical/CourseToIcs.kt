97f18c483d7e2ba2f29cfd490fe24ee3da7fb84e
zsqw123 <zsqw123@users.noreply.github.com>
2019-09-05 12:19:02 +0800

    支持导出ICS 已设置入口 功能部分实现

---
+++ app/src/main/java/com/jxxt/sues/ical/CourseToIcs.kt
@@ -0,0 +1,35 @@
+package com.jxxt.sues.ical
+
+import com.jxxt.sues.Item
+import com.jxxt.sues.widget.Utils
+import net.fortuna.ical4j.data.CalendarOutputter
+import net.fortuna.ical4j.model.*
+import net.fortuna.ical4j.model.Calendar
+import net.fortuna.ical4j.model.component.VEvent
+import net.fortuna.ical4j.model.property.*
+import java.io.File
+import java.io.FileOutputStream
+
+
+fun exIcs(list: List<Item>) {
+    // 创建日历
+    val calendar = Calendar()
+    calendar.properties.add(ProdId("iCal4j 3.0.9//EN"))
+    calendar.properties.add(Version.VERSION_2_0)
+    calendar.properties.add(CalScale.GREGORIAN)
+    for (i in list) {
+        // 时间主题
+        val summary = i.name
+        // 新建普通事件
+        val event = VEvent(DateTime(i.date.time), DateTime(i.date.time + 5400000L), summary)
+        // 生成唯一标示
+        event.properties.add(Uid("iCal4j"))
+        // 添加事件
+        calendar.components.add(event)
+    }
+    val fout = FileOutputStream(File(Utils.getContext().filesDir, "/1.ics"))
+    val outputter = CalendarOutputter()
+    outputter.output(calendar, fout)
+}
+
+
